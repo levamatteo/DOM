418fae68f2d11717115eb40cb5f7c083caac3445
levamatteo <55562385+levamatteo@users.noreply.github.com>
2019-10-29 13:27:03 -0700

    final

--- final.js
+++ final.js
@@ -1,13 +1,36 @@
 let img;
+let img1;
+let img2;
+let button;
+let myError;
+let adBlock
+
 function preload() {
   img = loadImage('Assets/spamRisk.jpg');
+  img1 = loadImage('Assets/gmail.jpg');
+  img2 = loadImage('Assets/error.png');
+  soundFormats('mp3', 'ogg');
+  myError = loadSound('assets/errorSound.mp3');
 }
 
 function setup() {
-  createCanvas(400, 400);
- background(0)
+  adBlock = select('#adBlocked')
+  adBlock.hide();
+  createCanvas(windowWidth, windowHeight);
+  background(img1)
+  button = createButton('adBlocker');
+  button.position(1750, 40);
+  button.mousePressed(changeBG);
 }
 
 function draw() {
-    image(img, mouseX, mouseY);
+  image(img, mouseX, mouseY);
+}
+
+function changeBG() {
+
+  background(img2);
+  myError.setVolume(0.1);
+  myError.play();
+  adBlock.show();
 }
